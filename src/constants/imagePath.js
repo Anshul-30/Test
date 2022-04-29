94b01d32579b54fb0a430615105642d97142015e
CBL-Mac-030 <cbl-mac-030@CBL-Mac-030s-iMac-Pro.local>
2022-04-29 17:49:46 +0530

    changes

--- src/constants/imagePath.js
+++ src/constants/imagePath.js
@@ -6,5 +6,9 @@ export default{
     google : require('../assets/images/ic_google.png'),
     arrow :require('../assets/images/ic_back_arrow.png'),
     down_arrow: require('../assets/images/ic_down.png'),
-    Home:require('../assets/images/ic_home.png')
+    Home:require('../assets/images/ic_home.png'),
+    Notify:require('../assets/images/ic_notify.png'),
+    search:require('../assets/images/ic_srch.png'),
+    post:require('../assets/images/ic_plus.png'),
+    profile:require('../assets/images/ic_user.png')
 }
--- src/navigation/BottomTabNavigation.js
+++ src/navigation/BottomTabNavigation.js
@@ -6,35 +6,100 @@ import navigationStrings from './navigationStrings';
 import {Home, Notification, Post, Profile, Search} from '../Screens/Main';
 import images from '../constants/imagePath';
 import { moderateScale, width } from '../styles/responsiveSize';
+import colors from '../styles/colors';
 
 const Tab = createBottomTabNavigator();
 
 export default BottomTab = () => {
   return (
-    <Tab.Navigator screenOptions={{headerShown: false, tabBarShowLabel: false}}>
+    <Tab.Navigator screenOptions={{headerShown: false, tabBarShowLabel: false }}>
       <Tab.Screen
         name={navigationStrings.HOME}
         component={Home}
         options={{
-          tabBarIcon: ({focused}) => {
-            <Image source={images?.Home} style={{
-                height: moderateScale(width / 10),
-                width: moderateScale(width / 10),
+          tabBarIcon: ({focused}) => (
+            <Image
+              source={images.Home}
+              style={{
+                height: moderateScale(width / 20),
+                width: moderateScale(width / 20),
                 resizeMode: 'contain',
-                marginTop: 5,
-                tintColor: focused ? 'black' : 'grey',
-              }} />;
-          },
+                marginTop:moderateScale(10),
+                tintColor: focused ? 'red' : 'grey',
+              }}
+            />
+          ),
+          // tabBarItemStyle: {height: 40},
         }}
       />
-      <Tab.Screen name={navigationStrings.Search} component={Search} />
-      <Tab.Screen name={navigationStrings.Post} component={Post} />
+      <Tab.Screen name={navigationStrings.Search} component={Search} 
+       options={{
+        tabBarIcon: ({focused}) => (
+          <Image
+            source={images.search}
+            style={{
+              height: moderateScale(width / 20),
+              width: moderateScale(width / 20),
+              resizeMode: 'contain',
+              marginTop:moderateScale(10),
+              tintColor: focused ? 'red' : 'grey',
+            }}
+          />
+        ),
+        // tabBarItemStyle: {height: 40},
+      }}/>
+      <Tab.Screen name={navigationStrings.Post} component={Post}
+       options={{
+        tabBarIcon: ({focused}) => (
+          <Image
+            source={images.post}
+            style={{
+              height: moderateScale(width / 20),
+              width: moderateScale(width / 20),
+              resizeMode: 'contain',
+              marginTop:moderateScale(10),
+              tintColor: focused ? 'red' : 'grey',
+            }}
+          />
+        ),
+        // tabBarItemStyle: {height: 40},
+      }} />
       <Tab.Screen
         name={navigationStrings.Notification}
         component={Notification}
+        options={{
+          tabBarIcon: ({focused}) => (
+            <Image
+              source={images?.Notify}
+              style={{
+                height: moderateScale(width / 20),
+                width: moderateScale(width / 20),
+                resizeMode: 'contain',
+                marginTop:moderateScale(10),
+                tintColor: focused ? 'red' : 'grey',
+              }}
+            />
+          ),
+          // tabBarItemStyle: {height: 40},
+        }}
       />
 
-      <Tab.Screen name={navigationStrings.Profile} component={Profile} />
+      <Tab.Screen name={navigationStrings.Profile} component={Profile} 
+       options={{
+        tabBarIcon: ({focused}) => (
+          <Image
+            source={images.profile}
+            style={{
+              height: moderateScale(width / 20),
+              width: moderateScale(width / 20),
+              resizeMode: 'contain',
+              marginTop:moderateScale(10),
+              tintColor: focused ? 'red' : 'grey',
+            }}
+          />
+        ),
+        // tabBarItemStyle: {height: 40},
+      }}/>
     </Tab.Navigator>
   );
 };
