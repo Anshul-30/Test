254c5dbb3670a42162e6431f64f6c79558d49dd4
CBL-Mac-030 <cbl-mac-030@CBL-Mac-030s-iMac-Pro.local>
2022-05-18 18:58:12 +0530

    ActionSheet

--- src/Components/ActionSheetComponent.js
+++ src/Components/ActionSheetComponent.js
@@ -1,80 +1,81 @@
-import React, {useState,useEffect} from 'react';
+import React, {useState, useEffect} from 'react';
 import ActionSheet, {SheetManager} from 'react-native-actions-sheet';
 import {Text, View} from 'react-native';
 import TextInputComp from './TextInputComponent';
 import ButtonComponent from './ButtonComponent';
-import { height, moderateScale } from '../styles/responsiveSize';
+import {height, moderateScale} from '../styles/responsiveSize';
 import actions from '../redux/actions';
-import { FlatList } from 'react-native-gesture-handler';
+import {FlatList, TouchableWithoutFeedback} from 'react-native-gesture-handler';
+import {cloneDeep} from 'lodash';
 
 function ActionSheetComponent({addComment}) {
   const [data, setData] = useState();
-  const [comment, setComment]= useState()
-  const [count,setCount]= useState(0)
-  const state ={ data,comment}
-  console.log('state-------------',state)
+  const [comment, setComment] = useState();
+  const [allComment, setAllComment] = useState([]);
+
+  const state = {data, comment};
+  console.log('state-------------', state);
   console.log('data', data);
 
-useEffect(() => {
-    if(data){
-  getAllComment()}
+  useEffect(() => {
+    
+    //   getAllComment();
 
- 
-}, [data])
+  }, []);
 
-const getAllComment =()=>{
-    let apiData =  `?post_id=${data.value.item.id}&skip=${count}`
-    actions.getComment(apiData).then((res)=>{
-        console.log(res,"get comment response--------------")
-    }).catch((err)=>{
-        console.log(err)
-    })
-}
+  const getAllComment = () => {
+    let apiData = `?post_id=${data.value.item.id}`;
+    actions
+      .getComment(apiData)
+      .then(res => {
+        console.log(res, 'get comment response--------------');
+        
+        setAllComment(res.data);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
 
   return (
     <ActionSheet id="title" onBeforeShow={data => setData(data)}>
       {data ? (
         <View style={{height: '70%'}}>
-            
-          <Text>{data.value.item.id}</Text>
-           
-          <View style={{flexDirection: 'row',alignItems:'center'}}>
-            <View style={{flex:.7}}>
-              <TextInputComp placeholder='Add Comments' onChangeText={(data)=>setComment(data)} value={comment}/>
-            </View>
-            <View style={{flex:.3}}>
+          
 
-            <ButtonComponent title='Send' onpress={()=>addComment(state)} />
+          <View style={{flexDirection: 'row', alignItems: 'center'}}>
+            <View style={{flex: 0.7}}>
+              <TextInputComp
+                placeholder="Add Comments"
+                onChangeText={data => setComment(data)}
+                value={comment}
+              />
+            </View>
+            <View style={{flex: 0.3}}>
+              <ButtonComponent title="Send" onpress={() => addComment(state)} />
             </View>
           </View>
-            {data.value.item.commets? 
-            <FlatList
-            data={data.value.item.commets}
-            onEndReached={()=>{
-                console.log(count)
-                setCount(count+3)
-                
-            }}
-            onEndReachedThreshold={.9}
-            renderItem={(element)=>{
-                console.log('element',element)
-                return(
-                    <Text>{element.item.comment}</Text>
-                )
-            }}
-            
-            />
-            
-            // data.value.item.commets.map((i,inx)=>{
-            //     console.log(i)
-            //     return(
 
-            // <><View>
-            //     <Text>{i.comment}</Text>
-            //     </View></>
-            //     )
-            // })
-            :null}
+
+    <View style={{height:height}}>
+
+          <FlatList
+            data={allComment}
+            
+            
+            renderItem={element => {
+                // console.log(element)
+              return (
+                <View><View style={{flexDirection:'row' ,justifyContent:'space-between',margin:10}}>
+                  <Text>{element.item.comment}</Text>
+                  <Text>{'commented by'}{element.item.user.first_name}</Text>
+                    </View>
+                </View>
+              );
+            }}
+          />
+    </View>
+
         </View>
       ) : null}
     </ActionSheet>
